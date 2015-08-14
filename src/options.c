f826cfd3bba3ee0725c0121a658f72e8f202ccb6
Paul J. Lucas <plucas@splunk.com>
2015-08-14 10:38:23 -0700

    Added mutually exclusive and required option checks and tests.

--- src/options.c
+++ src/options.c
@@ -24,11 +24,16 @@
 #include "util.h"
 
 // standard
+#include <ctype.h>
 #include <stdlib.h>                     /* for exit() */
 #include <string.h>
 #include <unistd.h>                     /* for getopt() */
 
-///////////////////////////////////////////////////////////////////////////////
+#define GAVE_OPTION(OPT)  isalpha( OPTION_VALUE(OPT) )
+#define OPTION_VALUE(OPT) opts_given[ !islower(OPT) ][ toupper(OPT) - 'A' ]
+#define SET_OPTION(OPT)   OPTION_VALUE(OPT) = (OPT)
+
+////////// extern variables ///////////////////////////////////////////////////
 
 char const *doc_name;
 FILE       *fin;
@@ -44,13 +49,75 @@ bool        opt_no_check_doc;
 bool        opt_no_timestamp;
 bool        opt_verbose;
 
+////////// local variables ////////////////////////////////////////////////////
+
+static char opts_given[ 2 /* lower/upper */ ][ 26 + 1 /*NULL*/ ];
+
 ////////// local functions ////////////////////////////////////////////////////
 
+/**
+ * Checks that if \a opt was given, that at least one \a req_opts was also
+ * given.
+ * If not, prints an error message and exits.
+ *
+ * @param opt The short option.
+ * @param req_opts The set of required options for \a opt.
+ */
+static void check_required( char opt, char const *req_opts ) {
+  if ( GAVE_OPTION( opt ) ) {
+    for ( char const *req_opt = req_opts; *req_opt; ++req_opt )
+      if ( GAVE_OPTION( *req_opt ) )
+        return;
+    bool const reqs_multiple = strlen( req_opts ) > 1;
+    PMESSAGE_EXIT( USAGE,
+      "-%c requires %sthe -%s option%s to be given also\n",
+      opt,
+      (reqs_multiple ? "one of " : ""),
+      req_opts, (reqs_multiple ? "s" : "")
+    );
+  }
+}
+
+/**
+ * Checks that no options were given that are among the two given mutually
+ * exclusive sets of short options.
+ * Prints an error message and exits if any such options are found.
+ *
+ * @param opts1 The first set of short options.
+ * @param opts2 The second set of short options.
+ */
+static void check_mutually_exclusive( char const *opts1, char const *opts2 ) {
+  int gave_count = 0;
+  char const *opt = opts1;
+  char gave_opt1 = '\0';
+
+  for ( int i = 0; i < 2; ++i ) {
+    for ( ; *opt; ++opt ) {
+      if ( GAVE_OPTION( *opt ) ) {
+        if ( ++gave_count > 1 ) {
+          char const gave_opt2 = *opt;
+          PMESSAGE_EXIT( USAGE,
+            "-%c and -%c are mutually exclusive\n", gave_opt1, gave_opt2
+          );
+        }
+        gave_opt1 = *opt;
+        break;
+      }
+    } // for
+    if ( !gave_count )
+      break;
+    opt = opts2;
+  } // for
+}
+
+/**
+ * Prints the usage message to standard error and exits.
+ */
 static void usage( void ) {
   PRINT_ERR(
 "usage: %s [-bctv] document_name file.txt file.pdb\n"
-"       %s  -d  [-Dv] file.pdb [file.txt]\n"
-"       %s  -V\n"
+"       %s -d [-Dv] file.pdb [file.txt]\n"
+"       %s -V\n"
 "\n"
 "options:\n"
 " -b: Don't strip binary characters [default: do].\n"
@@ -75,6 +142,7 @@ void process_options( int argc, char *argv[] ) {
 
   opterr = 1;
   for ( int opt; (opt = getopt( argc, argv, opts )) != EOF; ) {
+    SET_OPTION( opt );
     switch ( opt ) {
       case 'b': opt_binary = false;                 break;
       case 'c': opt_compress = false;               break;
@@ -87,6 +155,13 @@ void process_options( int argc, char *argv[] ) {
     } // switch
   } // for
   argc -= optind, argv += optind - 1;
+
+  // check for mutually exclusive options
+  check_mutually_exclusive( "bct", "dD" );
+  check_mutually_exclusive( "V", "bcdDtv" );
+
+  // check for options that require other options
+  check_required( 'D', "d" );
 
   if ( opt_decode ) {
     switch ( argc ) {
