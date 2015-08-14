0ab953ecd6418c322bdcb520e4e6d876f36ef4af
Paul J. Lucas <plucas@splunk.com>
2015-08-13 22:03:24 -0700

    Preliminary tests.

---
+++ test/run_test.sh
@@ -0,0 +1,246 @@
+#! /bin/sh
+##
+#       txt2pdbdoc -- Text to Doc converter for Palm Pilots
+#       test/run_test.sh
+#
+#       Copyright (C) 2015  Paul J. Lucas
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the Licence, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+# Uncomment the following line for shell tracing.
+#set -x
+
+########## Functions ##########################################################
+
+local_basename() {
+  ##
+  # Autoconf, 11.15:
+  #
+  # basename
+  #   Not all hosts have a working basename. You can use expr instead.
+  ##
+  expr "//$1" : '.*/\(.*\)'
+}
+
+pass() {
+  print_result PASS $TEST_NAME
+  {
+    echo ":test-result: PASS"
+    echo ":copy-in-global-log: no"
+  } > $TRS_FILE
+}
+
+fail() {
+  print_result FAIL $TEST_NAME
+  {
+    echo ":test-result: FAIL"
+    echo ":copy-in-global-log: yes"
+  } > $TRS_FILE
+  exit $ACTUAL_EXIT
+}
+
+print_result() {
+  RESULT=$1; shift
+  MSG="$RESULT: $*"
+  COLOR=`eval echo \\$COLOR_$RESULT`
+  if [ "$COLOR" ]
+  then echo $COLOR$MSG$COLOR_NONE
+  else echo $MSG
+  fi
+}
+
+usage() {
+  [ "$1" ] && { echo "$ME: $*" >&2; usage; }
+  cat >&2 <<END
+usage: $ME --test-name=NAME --log-file=PATH --trs-file=PATH [options] TEST-COMMAND
+options:
+  --color-tests={yes|no}
+  --enable-hard-errors={yes|no}
+  --expect-failure={yes|no}
+END
+  exit 1
+}
+
+########## Begin ##############################################################
+
+ME=`local_basename "$0"`
+
+[ "$BUILD_SRC" ] || {
+  echo "$ME: \$BUILD_SRC not set" >&2
+  exit 2
+}
+
+########## Process command-line ###############################################
+
+while [ $# -gt 0 ]
+do
+  case $1 in
+  --color-tests)
+    COLOR_TESTS=$2; shift
+    ;;
+  --color-tests=*)
+    COLOR_TESTS=`expr "x$1" : 'x--color-tests=\(.*\)'`
+    ;;
+  --enable-hard-errors)
+    ENABLE_HARD_ERRORS=$2; shift
+    ;;
+  --enable-hard-errors=*)
+    ENABLE_HARD_ERRORS=`expr "x$1" : 'x--enable-hard-errors=\(.*\)'`
+    ;;
+  --expect-failure)
+    EXPECT_FAILURE=$2; shift
+    ;;
+  --expect-failure=*)
+    EXPECT_FAILURE=`expr "x$1" : 'x--expect-failure=\(.*\)'`
+    ;;
+  --help)
+    usage
+    ;;
+  --log-file)
+    LOG_FILE=$2; shift
+    ;;
+  --log-file=*)
+    LOG_FILE=`expr "x$1" : 'x--log-file=\(.*\)'`
+    ;;
+  --test-name)
+    TEST_NAME=$2; shift
+    ;;
+  --test-name=*)
+    TEST_NAME=`expr "x$1" : 'x--test-name=\(.*\)'`
+    ;;
+  --trs-file)
+    TRS_FILE=$2; shift
+    ;;
+  --trs-file=*)
+    TRS_FILE=`expr "x$1" : 'x--trs-file=\(.*\)'`
+    ;;
+  --)
+    shift
+    break
+    ;;
+  -*)
+    usage
+    ;;
+  *)
+    break
+    ;;
+  esac
+  shift
+done
+
+[ "$TEST_NAME" ] || usage "required --test-name not given"
+[ "$LOG_FILE"  ] || usage "required --log-file not given"
+[ "$TRS_FILE"  ] || usage "required --trs-file not given"
+[ $# -ge 1     ] || usage "required test-command not given"
+TEST=$1
+
+########## Initialize #########################################################
+
+if [ "$COLOR_TESTS" = yes ]
+then
+  COLOR_BLUE="[1;34m"
+  COLOR_GREEN="[0;32m"
+  COLOR_LIGHT_GREEN="[1;32m"
+  COLOR_MAGENTA="[0;35m"
+  COLOR_NONE="[m"
+  COLOR_RED="[0;31m"
+
+  COLOR_ERROR=$COLOR_MAGENTA
+  COLOR_FAIL=$COLOR_RED
+  COLOR_PASS=$COLOR_GREEN
+  COLOR_SKIP=$COLOR_BLUE
+  COLOR_XFAIL=$COLOR_LIGHT_GREEN
+  COLOR_XPASS=$COLOR_RED
+fi
+
+case $EXPECT_FAILURE in
+yes) EXPECT_FAILURE=1 ;;
+  *) EXPECT_FAILURE=0 ;;
+esac
+
+##
+# The automake framework sets $srcdir. If it's empty, it means this script was
+# called by hand, so set it ourselves.
+##
+[ "$srcdir" ] || srcdir="."
+
+DATA_DIR=$srcdir/data
+EXPECTED_DIR=$srcdir/expected
+TEST_NAME=`local_basename "$TEST_NAME"`
+OUTPUT=/tmp/ad_test_output_$$_
+
+########## Run test ###########################################################
+
+run_sh_file() {
+  if $TEST $OUTPUT $LOG_FILE
+  then pass
+  else fail
+  fi
+}
+
+run_test_file() {
+  IFS='|' read COMMAND OPTIONS DOC_NAME INPUT EXPECTED_EXIT < $TEST
+  COMMAND=`echo $COMMAND`               # trims whitespace
+  DOC_NAME=`echo $DOC_NAME`             # trims whitespace
+  INPUT=$DATA_DIR/`echo $INPUT`         # trims whitespace
+  OUTFILE=`echo $OUTFILE`               # trims whitespace
+  EXPECTED_EXIT=`echo $EXPECTED_EXIT`   # trims whitespace
+
+  $COMMAND $OPTIONS "$DOC_NAME" $INPUT $OUTPUT 2> $LOG_FILE
+  ACTUAL_EXIT=$?
+
+  if [ $ACTUAL_EXIT -eq 0 ]
+  then                                  # success: diff output file
+    if [ 0 -eq $EXPECTED_EXIT ]
+    then
+      EXPECTED_TXT="$EXPECTED_DIR/`echo $TEST_NAME | sed 's/test$/txt/'`"
+      EXPECTED_BIN="$EXPECTED_DIR/`echo $TEST_NAME | sed 's/test$/pdb/'`"
+      if [ -f $EXPECTED_TXT ]
+      then EXPECTED_OUTPUT=$EXPECTED_TXT
+      else EXPECTED_OUTPUT=$EXPECTED_BIN
+      fi
+      if diff $EXPECTED_OUTPUT $OUTPUT >> $LOG_FILE
+      then pass; mv $OUTPUT $LOG_FILE
+      else fail
+      fi
+    else
+      fail
+    fi
+  else                                  # failure: expected exit status?
+    if [ $ACTUAL_EXIT -eq $EXPECTED_EXIT ]
+    then pass
+    else fail
+    fi
+  fi
+}
+
+##
+# Must put BUILD_SRC first in PATH so we get the correct version of ad.
+##
+PATH=$BUILD_SRC:$PATH
+
+##
+# Must unset these so we get the default colors in test output.
+##
+unset AD_COLORS GREP_COLOR GREP_COLORS
+
+trap "x=$?; rm -f /tmp/*_$$_* 2>/dev/null; exit $x" EXIT HUP INT TERM
+
+case $TEST in
+*.sh)   run_sh_file ;;
+*.test) run_test_file ;;
+esac
+
+# vim:set et sw=2 ts=2:
---
+++ test/tests/README.md
@@ -0,0 +1,48 @@
+Test File Format
+================
+
+Test files (ending in `.test`) must be a single line in the following format:
+
+*command* `|` *options* `|` *doc_name* `|` *input* `|` *exit*
+
+that is five fields separated by the pipe (`|`) character
+(with optional whitespace)
+where:
+
+1. *command*  = command to execute (`txt2pdbdoc`)
+2. *options*  = command-line options or blank for none
+3. *doc_name* = name of file to dump
+4. *input*    = name of file to dump
+5. *exit*     = expected exit status code
+
+Test scripts (ending in `.sh`) can alternatively be used
+when additional commands need to be executed as part of the test.
+Scripts are called with two command-line arguments:
+
+1. The full path of the test output file.
+2. The full path of the log file.
+
+Test scripts must follow the normal Unix convention
+of exiting with zero on success and non-zero on failure.
+
+Note on Test Names
+------------------
+
+Care must be taken when naming files that differ only in case
+because of case-insensitive (but preserving) filesystems like HFS+
+used on Mac OS X.
+
+For example, tests such as these:
+
+    ad-B2-e1-m.test
+    ad-B2-E1-m.test
+
+that are the same test but differ only in 'e' vs. 'E' will work fine
+on every other Unix filesystem but cause a collision on HFS+.
+
+One solution (the one used here) is to append a distinct number:
+
+    ad-B2-e1-m_01.test
+    ad-B2-E1-m_02.test
+
+thus making the filenames unique regardless of case.
