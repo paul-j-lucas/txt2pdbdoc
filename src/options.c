07a7117dc421666a4911f85ee45c46f4bed5b13f
Paul J. Lucas <plucas@splunk.com>
2015-08-28 19:56:09 -0700

    Made check_required() for symmetical.

--- src/options.c
+++ src/options.c
@@ -38,15 +38,15 @@ void check_required( char const *opts, char const *req_opts ) {
   assert( opts );
   assert( req_opts );
 
-  for ( ; *opts; ++opts ) {
-    if ( GAVE_OPTION( *opts ) ) {
+  for ( char const *opt = opts; *opt; ++opt ) {
+    if ( GAVE_OPTION( *opt ) ) {
       for ( char const *req_opt = req_opts; *req_opt; ++req_opt )
         if ( GAVE_OPTION( *req_opt ) )
           return;
       bool const reqs_multiple = strlen( req_opts ) > 1;
       PMESSAGE_EXIT( USAGE,
         "-%c requires %sthe -%s option%s to be given also\n",
-        *opts, (reqs_multiple ? "one of " : ""),
+        *opt, (reqs_multiple ? "one of " : ""),
         req_opts, (reqs_multiple ? "s" : "")
       );
     }
